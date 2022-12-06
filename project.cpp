d9dc68d481480a3ca9acd795866e23b35b6f6ff4
SirCutMonkee <113376917+SirCutMonkee@users.noreply.github.com>
2022-12-06 09:20:50 -0500

    Update project.cpp
    Added use of GPIO.h and Analogin.h

--- project.cpp
+++ project.cpp
@@ -17,44 +17,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "MQTTClient.h"
+#include "GPIO.h"
+#include "Analogin.h"
 using namespace std;
+using namespace exploringBB;
 
 #define ADDRESS     "tcp://io.adafruit.com:1883"
 #define CLIENTID    "Josh_Beagle"
 #define TOPIC1      "JoshuaRyan99/feeds/weather.temperature"
 #define TOPIC2      "JoshuaRyan99/feeds/weather.heater"
-#define TOPIC3      "JoshuaRyan99/feeds/weather.air-conditioner"
+#define TOPIC3      "JoshuaRyan99/feeds/weather.led"
+#define TOPIC4      "JoshuaRyan99/feeds/weather.button"
 #define QOS         1
 #define TIMEOUT     10000L
-#define LED_GPIO    "/sys/class/gpio/"
-#define ADC_PATH    "/sys/bus/iio/devices/iio:device0/in_voltage"
-#define ADC         0
-
-
-// Use this function to control the LED
-void writeGPIO(string gpio, string filename, string value){
-   fstream fs;
-   string path(LED_GPIO);
-   fs.open((path + gpio + "/" + filename).c_str(), fstream::out);
-   fs << value;
-   fs.close();
-}
-
-float getTemperature(int adc_value) {     // from the TMP36 datasheet
-   float cur_voltage = adc_value * (1.80f/4096.0f); // Vcc = 1.8V, 12-bit
-   float diff_degreesC = (cur_voltage-0.75f)/0.01f;
-   return (25.0f + diff_degreesC);
-}
-
-int readAnalog(int number){
-   stringstream ss;
-   ss << ADC_PATH << number << "_raw";
-   fstream fs;
-   fs.open(ss.str().c_str(), fstream::in);
-   fs >> number;
-   fs.close();
-   return number;
-}
+//#define LED_GPIO    "/sys/class/gpio/"
+//#define ADC_PATH    "/sys/bus/iio/devices/iio:device0/in_voltage"
+//#define ADC         0
 
 volatile MQTTClient_deliveryToken deliveredtoken;
 
@@ -67,11 +45,11 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
     char* payloadptr;
     payloadptr = (char*) message->payload;
     int switchState = atoi(payloadptr);
-    if(strcmp(topicName, TOPIC2) == 0){
-        switchState == 1 ? writeGPIO("gpio60", "value", "1") : writeGPIO("gpio60", "value", "0");
+    if(strcmp(topicName, TOPIC3) == 0){
+        switchState == 1 ? led.setValue(HIGH) : led.setValue(LOW);
     }
     else{
-        switchState == 1 ? writeGPIO("gpio48", "value", "1") : writeGPIO("gpio48", "value", "0");
+        switchState == 1 ? heater.setValue(HIGH) : heater.setValue(LOW);
     }
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
@@ -83,11 +61,23 @@ void connlost(void *context, char *cause) {
     printf("     cause: %s\n", cause);
 }
 
-int main(int argc, char* argv[]) {
-    writeGPIO("gpio60", "direction", "out");
-    writeGPIO("gpio48", "direction", "out");
+void publish(const char *topicName, int val){
     int rc;
-    int ch;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;    
+    sprintf(str_payload, "%f", val);    
+    pubmsg.payload = str_payload;
+    pubmsg.payloadlen = strlen(str_payload);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, topicName, &pubmsg, &token);
+    if(rc = MQTTClient_waitForCompletion(client, token, TIMEOUT) != MQTTCLIENT_SUCCESS){
+        exit(-1);
+    }
+}
+
+void subscribe(MQTTClient client;){
+    int rc;
     char str_payload[100];
     string tpc2 = TOPIC2;
     string tpc3 = TOPIC3;
@@ -96,42 +86,46 @@ int main(int argc, char* argv[]) {
     int qos2 = 1;
     int qos3 = 1;
     int qosArray[2] = {qos2, qos3};
-
-    MQTTClient client;
     MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
     MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
     opts.keepAliveInterval = 0;
     opts.cleansession = 1;
     opts.username = getenv("IO_USERNAME");
     opts.password = getenv("IO_KEY");
     MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);   
-    
     if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
         exit(-1);
-    }
-    
+    }    
     MQTTClient_subscribeMany(client, 2, topicArray, qosArray);
+}
 
+int main(int argc, char* argv[]) {    
     int cntr = 0;
+    AnalogIn tempSensor(0);
+    GPIO led(60);
+    GPIO heater(48);
+    GPIO button(46);
+    
+    led.setDirection(OUTPUT);
+    heater.setDirection(OUTPUT);
+    button.setDirection(INPUT);
+    GPIO_VALUE lastState;
+    GPIO_VALUE currentState;
+    currentState = button.getValue();
+    lastState = !currentState;
+
+    MQTTClient client;    
+    subscribe(client);
+    
     while(1){
+        if(currentState != lastState){
+            publish(TOPIC4, currentState);
+            lastState = currentState;
+        }
+        currentState = button.getValue();
+        
         if(cntr >=100000){
-           sprintf(str_payload, "%f", getTemperature(readAnalog(ADC)));
-           pubmsg.payload = str_payload;
-           pubmsg.payloadlen = strlen(str_payload);
-           pubmsg.qos = QOS;
-           pubmsg.retained = 0;
-           MQTTClient_publishMessage(client, TOPIC1, &pubmsg, &token);
-           rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-//           if(rc != MQTTCLIENT_SUCCESS) {
-//               cout << "Did not complete with error code: " << rc << endl;
-               // MQTTCLIENT_SUCCESS 0           MQTTCLIENT_FAILURE -1
-               // MQTTCLIENT_DISCONNECTED -3     MQTTCLIENT_MAX_MESSAGES_INFLIGHT -4
-               // MQTTCLIENT_BAD_UTF8_STRING -5  MQTTCLIENT_NULL_PARAMETER -6
-               // MQTTCLIENT_TOPICNAME_TRUNCATED -7   MQTTCLIENT_BAD_STRUCTURE -8
-               // MQTTCLIENT_BAD_QOS   -9        MQTTCLIENT_SSL_NOT_SUPPORTED   -10
-//           }
+           publish(TOPIC1, tempSensor.readADCSample());
            cntr = 0;
         }
         else{
